110c48703154ff99d463b31291ee83eabea4168b
gimpei <gim.kobayashi@gmail.com>
2017-12-19 11:50:53 +0900

    a

--- gcf-pythonic-aws-observe/aws_lambda_driver/lambda_function.py
+++ gcf-pythonic-aws-observe/aws_lambda_driver/lambda_function.py
@@ -3,4 +3,5 @@ import os
 SECRET = os.environ['SECRET']
 def lambda_handler(event, context):
     res = requests.get('https://us-central1-wild-yukikaze.cloudfunctions.net/pycall_aws_observe?secret='  + SECRET)
+    res = requests.get('https://us-central1-wild-yukikaze.cloudfunctions.net/pycall_aws_network?secret='  + SECRET)
     return res.status_code
